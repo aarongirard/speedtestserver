49e02be1b0b74d3ca55e5a981161b9c75c7f5d4b
aarongirard <aarongirard95@gmail.com>
2016-06-20 23:08:40 -0400

    set up an initial server

--- speedtestserver.py
+++ speedtestserver.py
@@ -6,3 +6,9 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello_world():
+    return 'BOOBS'
+
+#To set to a specific ip app.run(host="192.168.1.7",port=5010) handy if your pc has a few ip's
+app.run(host="192.168.0.39", port = 5000)
