4164703988d17af328977368d9aba0f4b5442e8f
Aaron Girard <aarongirard95@gmail.com>
2016-06-29 23:52:55 -0400

    added method to parse tests sql

--- speedtestserver.py
+++ speedtestserver.py
@@ -8,6 +8,19 @@ app = Flask(__name__)
 
 DB = 'SpeedTest.db'
 
+def process_tests(tests):
+	date = []
+	download = []
+	upload = []
+	ping = []
+	for test in tests:
+		date.append(test[0])
+		download.append(test[1])
+		upload.append(test[2])
+		ping.append(test[3])
+	return [date,download,upload,ping]
+
+
 def get_db():
 	db = getattr(g, '_database', None)
 	if db is None:
@@ -29,6 +42,7 @@ def request_speed_tests():
 	db = get_db()
 	cursor = db.execute('select * from tests;')
 	tests  = cursor.fetchall()
+	process_tests(tests) #tests will be a tuple time, downlaod, uploadd, ping
 	return render_template('index.html',tests=tests)	
 
 #To set to a specific ip app.run(host="192.168.1.7",port=5010) handy if your pc has a few ip's
